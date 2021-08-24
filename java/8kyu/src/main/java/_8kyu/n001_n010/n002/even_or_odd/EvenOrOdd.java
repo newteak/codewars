274b2726365485a4cbec66365c32c90578834edb
Taeksoo Shin <cu7898@gmail.com>
2021-08-25 08:49:30 +0900

    Feat add javadoc

--- java/8kyu/src/main/java/_8kyu/n001_n010/n002/even_or_odd/EvenOrOdd.java
+++ java/8kyu/src/main/java/_8kyu/n001_n010/n002/even_or_odd/EvenOrOdd.java
@@ -1,6 +1,14 @@
 package _8kyu.n001_n010.n002.even_or_odd;
 
 public class EvenOrOdd {
+    /**
+     * Judge param is even or odd.
+     *
+     *
+     * @param number - judged num
+     *
+     * @return when param is even return "Even",and param is odd return "Odd"
+     */
     public static String evenOrOdd(int number) {
         return (number % 2 == 0) ? "Even" : "Odd";
     }
