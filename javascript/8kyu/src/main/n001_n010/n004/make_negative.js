9e8f6285a9214141806f47bd496d7dcc49f8def5
Taeksoo Shin <cu7898@gmail.com>
2021-12-05 10:40:29 +0900

    Ref make logic more simple
    [Explain]
    1. arrow function doesn't need to use return on specific situation
    
    issue: #1

--- javascript/8kyu/src/main/n001_n010/n004/make_negative.js
+++ javascript/8kyu/src/main/n001_n010/n004/make_negative.js
@@ -3,8 +3,6 @@
 * URL: https://www.codewars.com/kata/55685cd7ad70877c23000102/train/javascript
 *
 */
-const makeNegative = (num) => {
-  return num < 0 ? num : -num;
-};
+const makeNegative = (num) => num < 0 ? num : -num;
 
 export default makeNegative;
