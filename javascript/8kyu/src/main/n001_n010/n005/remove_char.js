8a6171e28906da851ad161ecab7190571f8b04ec
Taeksoo Shin <cu7898@gmail.com>
2021-12-06 21:50:09 +0900

    Feat green - use substring
    issue: #3

--- javascript/8kyu/src/main/n001_n010/n005/remove_char.js
+++ javascript/8kyu/src/main/n001_n010/n005/remove_char.js
@@ -3,6 +3,6 @@
 * URL: https://www.codewars.com/kata/56bc28ad5bdaeb48760009b0/train/javascript
 *
 */
-const removeChar = () => 'har';
+const removeChar = (str) => str.substring(1);
 
 export default removeChar;
