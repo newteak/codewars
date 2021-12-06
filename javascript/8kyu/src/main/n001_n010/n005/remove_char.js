1f6e498afd35b10215197d64bace38f037def790
Taeksoo Shin <cu7898@gmail.com>
2021-12-06 21:53:45 +0900

    Feat green - use String.prototype.lenght
    issue: #3

--- javascript/8kyu/src/main/n001_n010/n005/remove_char.js
+++ javascript/8kyu/src/main/n001_n010/n005/remove_char.js
@@ -3,6 +3,6 @@
 * URL: https://www.codewars.com/kata/56bc28ad5bdaeb48760009b0/train/javascript
 *
 */
-const removeChar = (str) => str.substring(1);
+const removeChar = (str) => str.substring(1, str.length - 1);
 
 export default removeChar;
