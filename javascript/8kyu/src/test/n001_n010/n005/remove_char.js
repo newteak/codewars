f7f70e6b2205c496185105bbf4ea1d2f7dca1bca
Taeksoo Shin <cu7898@gmail.com>
2021-12-06 21:51:34 +0900

    Docs fix comment
    issue: #3

--- javascript/8kyu/src/test/n001_n010/n005/remove_char.js
+++ javascript/8kyu/src/test/n001_n010/n005/remove_char.js
@@ -3,8 +3,8 @@ import {expect} from 'chai';
 import removeChar from '../../../main/n001_n010/n005/remove_char.js';
 
 // URL: https://www.codewars.com/kata/56bc28ad5bdaeb48760009b0/train/javascript
-// step1 - removeChar can remove first char
-// step2 - removeChar can remove last char
+// [x] step1 - removeChar can remove first char
+// [ ] step2 - removeChar can remove last char
 describe('Function removeChar', () => {
   context(`With ('char')`, () => {
     it (`Return 'har'`, () => {
