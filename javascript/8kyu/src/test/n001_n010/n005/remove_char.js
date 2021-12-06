5744fcaab5c9880adc5f836a4cd8b1fd791146a7
Taeksoo Shin <cu7898@gmail.com>
2021-12-06 21:55:58 +0900

    Docs edit comment
    issue: #3

--- javascript/8kyu/src/test/n001_n010/n005/remove_char.js
+++ javascript/8kyu/src/test/n001_n010/n005/remove_char.js
@@ -4,7 +4,7 @@ import removeChar from '../../../main/n001_n010/n005/remove_char.js';
 
 // URL: https://www.codewars.com/kata/56bc28ad5bdaeb48760009b0/train/javascript
 // [x] step1 - removeChar can remove first char
-// [ ] step2 - removeChar can remove last char
+// [x] step2 - removeChar can remove last char
 describe('Function removeChar', () => {
   context(`With ('char')`, () => {
     it (`Return 'ha'`, () => {
