6a3f7e9cc6b055664848809ec508820aec724aa1
Taeksoo Shin <cu7898@gmail.com>
2021-12-16 21:41:13 +0900

    Init Convert boolean values to strings 'Yes' or 'No'.
    issue: #5

---
+++ javascript/8kyu/src/main/n001_n010/n006/bool_to_word.js
@@ -0,0 +1,13 @@
+/**
+* boolToWord return a "Yes" string for true, or a "No" string for false.
+* URL: https://www.codewars.com/kata/53369039d7ab3ac506000467/train/javascript
+*
+*
+* @param {Boolean} bool - any boolean.
+*
+* @return {String} a "Yes" string for true, or a "No" string for false.
+*
+*/
+const boolToWord = (bool) => "";
+
+export default boolToWord;
---
+++ javascript/8kyu/src/test/n001_n010/n006/bool_to_word.test.js
@@ -0,0 +1,13 @@
+import {describe, it} from 'mocha';
+import {expect} from 'chai';
+import boolToWord from '../../../main/n001_n010/n006/bool_to_word.js';
+
+// URL: https://www.codewars.com/kata/53369039d7ab3ac506000467/train/javascript
+// [ ] step1 -
+describe('Function removeChar', () => {
+  context(`With ('hi')`, () => {
+    it (`Return ''`, () => {
+      expect('').to.equal('');
+    });
+  });
+});
