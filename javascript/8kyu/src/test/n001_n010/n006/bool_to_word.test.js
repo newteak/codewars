463727b3dc0b1b177c8bfc67f522ff9beda8fb66
Taeksoo Shin <cu7898@gmail.com>
2021-12-16 21:44:28 +0900

    Docs add goal
    [Explain]
    1. boolToWord(true) return 'Yes'
    
    issue: #5

--- javascript/8kyu/src/test/n001_n010/n006/bool_to_word.test.js
+++ javascript/8kyu/src/test/n001_n010/n006/bool_to_word.test.js
@@ -3,7 +3,7 @@ import {expect} from 'chai';
 import boolToWord from '../../../main/n001_n010/n006/bool_to_word.js';
 
 // URL: https://www.codewars.com/kata/53369039d7ab3ac506000467/train/javascript
-// [ ] step1 -
+// [ ] step1 - boolToWord(true) return 'Yes'
 describe('Function boolToWord', () => {
   context(`With (true)`, () => {
     it (`Return 'Yes'`, () => {
