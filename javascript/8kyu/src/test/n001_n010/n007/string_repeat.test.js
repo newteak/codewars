5af1966424786998e3f2221e8c44224863a97639
Taeksoo Shin <cu7898@gmail.com>
2021-12-20 09:06:13 +0900

    Feat red - With (2, "I") return 'II'
    issue: #7

--- javascript/8kyu/src/test/n001_n010/n007/string_repeat.test.js
+++ javascript/8kyu/src/test/n001_n010/n007/string_repeat.test.js
@@ -3,7 +3,7 @@ import {expect} from 'chai';
 import repeatStr from '../../../main/n001_n010/n007/string_repeat.js';
 
 // URL: https://www.codewars.com/kata/57a0e5c372292dd76d000d7e/train/javascript
-// [ ] step1 - with (1, str) will return str
+// [x] step1 - with (1, str) will return str
 // [ ] step2 - with (more than 1, str) will return str * number(repeat count is more than one)
 describe('Function repeatStr', () => {
   context(`With (1, "I")`, () => {
@@ -15,6 +15,12 @@ describe('Function repeatStr', () => {
   context(`With (1, "ABC")`, () => {
     it (`Return 'ABC'`, () => {
       expect('ABC').to.equal(repeatStr(1, "ABC"));
+    });
+  });
+
+  context(`With (2, "I")`, () => {
+    it (`Return 'II'`, () => {
+      expect('II').to.equal(repeatStr(2, "I"));
     });
   });
 });
