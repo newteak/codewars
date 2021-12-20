0a071aade3c91b01bacee8338f78ad6700662bcc
Taeksoo Shin <cu7898@gmail.com>
2021-12-20 09:26:13 +0900

    Style "" => ''
    issue: #7

--- javascript/8kyu/src/test/n001_n010/n007/string_repeat.test.js
+++ javascript/8kyu/src/test/n001_n010/n007/string_repeat.test.js
@@ -6,21 +6,27 @@ import repeatStr from '../../../main/n001_n010/n007/string_repeat.js';
 // [x] step1 - with (1, str) will return str
 // [ ] step2 - with (more than 1, str) will return str * number(repeat count is more than one)
 describe('Function repeatStr', () => {
-  context(`With (1, "I")`, () => {
+  context(`With (1, 'I')`, () => {
     it (`Return 'I'`, () => {
-      expect('I').to.equal(repeatStr(1, "I"));
+      expect('I').to.equal(repeatStr(1, 'I'));
     });
   });
 
-  context(`With (1, "ABC")`, () => {
+  context(`With (1, 'ABC')`, () => {
     it (`Return 'ABC'`, () => {
-      expect('ABC').to.equal(repeatStr(1, "ABC"));
+      expect('ABC').to.equal(repeatStr(1, 'ABC'));
     });
   });
 
-  context(`With (2, "I")`, () => {
+  context(`With (2, 'I')`, () => {
     it (`Return 'II'`, () => {
-      expect('II').to.equal(repeatStr(2, "I"));
+      expect('II').to.equal(repeatStr(2, 'I'));
+    });
+  });
+
+  context(`With (3, 'ABC ')`, () => {
+    it (`Return 'ABC ABC ABC '`, () => {
+      expect('ABC ABC ABC ').to.equal(repeatStr(3, 'ABC '));
     });
   });
 });
