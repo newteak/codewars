237f4f5002be13d38f6363c8e373189bc4624dac
Taeksoo Shin <cu7898@gmail.com>
2021-12-23 09:19:32 +0900

    Init Reversed Strings
    issue: #9

---
+++ javascript/8kyu/src/test/n001_n010/n008/reversed_strings.test.js
@@ -0,0 +1,12 @@
+import {describe, it} from 'mocha';
+import {expect} from 'chai';
+import reversedString from '../../../main/n001_n010/n008/reversed_string.js';
+
+// URL: https://www.codewars.com/kata/5168bb5dfe9a00b126000018/train/javascript
+describe('Function reversedString', () => {
+  context(`With ('h')`, () => {
+    it (`Return 'h'`, () => {
+      expect('h').to.equal(reversedString('h'));
+    });
+  });
+});
