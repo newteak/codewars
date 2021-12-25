72f8314af7411e946f6b72627d4148fbf912e97a
Taeksoo Shin <cu7898@gmail.com>
2021-12-25 12:29:42 +0900

    Feat add test - min, max
    issue: #11

--- javascript/8kyu/src/test/n001_n010/n009/number_to_string.test.js
+++ javascript/8kyu/src/test/n001_n010/n009/number_to_string.test.js
@@ -4,6 +4,12 @@ import numberToString from '../../../main/n001_n010/n009/number_to_string.js';
 
 // URL: https://www.codewars.com/kata/5265326f5fda8eb1160004c8/train/javascript
 describe('Function numberToString', () => {
+  context(`With (Number.MAX_VALUE)`, () => {
+    it (`Return '1.7976931348623157e+308'`, () => {
+      expect('1.7976931348623157e+308').to.equal(numberToString(Number.MAX_VALUE));
+    });
+  });
+
   context(`With (1)`, () => {
     it (`Return '1'`, () => {
       expect('1').to.equal(numberToString(1));
@@ -19,6 +25,13 @@ describe('Function numberToString', () => {
   context(`With (-1)`, () => {
     it (`Return '-1'`, () => {
       expect('-1').to.equal(numberToString(-1));
+    });
+  });
+
+  context(`With (Number.NEGATIVE_INFINITY)`, () => {
+    it (`Return '-Infinity'`, () => {
+      console.log(Number.MIN_VALUE);
+      expect('-Infinity').to.equal(numberToString(Number.NEGATIVE_INFINITY));
     });
   });
 });
