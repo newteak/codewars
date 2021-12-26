5c77926583e715d3999e38cd3a2fea7aab2ddfab
Taeksoo Shin <cu7898@gmail.com>
2021-12-26 19:07:43 +0900

    Style edit indent
    issue: #13

--- javascript/8kyu/src/main/n001_n010/n010/summation.js
+++ javascript/8kyu/src/main/n001_n010/n010/summation.js
@@ -1,13 +1,13 @@
 /**
-* summation finds the summation of every number from 1 to num
-* URL: https://www.codewars.com/kata/55d24f55d7dd296eb9000030/train/javascript
-*
-*
-* @param {Number} num - number is greater than 0.
-*
-* @return {Number} the summation of every number from 1 to num.
-*
-*/
+ * summation finds the summation of every number from 1 to num
+ * URL: https://www.codewars.com/kata/55d24f55d7dd296eb9000030/train/javascript
+ *
+ *
+ * @param {Number} num - number is greater than 0.
+ *
+ * @return {Number} the summation of every number from 1 to num.
+ *
+ */
 const summation = (num) => [...Array(num).keys(), num].reduce((prev, curr) => prev + curr, 0);
 
 export default summation;
--- javascript/8kyu/src/test/n001_n010/n010/summation.test.js
+++ javascript/8kyu/src/test/n001_n010/n010/summation.test.js
@@ -5,13 +5,13 @@ import summation from '../../../main/n001_n010/n010/summation.js';
 // URL: https://www.codewars.com/kata/55d24f55d7dd296eb9000030/train/javascript
 describe('Function summation', () => {
   context(`With (1)`, () => {
-    it (`Return 1`, () => {
+    it(`Return 1`, () => {
       expect(1).to.equal(summation(1));
     });
   });
 
   context(`With (2)`, () => {
-    it (`Return 1 + 2`, () => {
+    it(`Return 1 + 2`, () => {
       expect(1 + 2).to.equal(summation(2));
     });
   });
